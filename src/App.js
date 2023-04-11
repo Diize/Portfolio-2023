e4478f938ce3611102682804bc68b0b35a3891aa
Diize <51971449+Diize@users.noreply.github.com>
2023-04-11 21:10:34 +0200

    MAIN - first initial setup. Removed unnecessary stuff, added some dependencies like Mantine, React Router Dom etc.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { MantineProvider, Title, Center, Text, Stack } from '@mantine/core';
+import { BsArrowDown } from 'react-icons/bs';
+import { FaArrowDown } from 'react-icons/fa';
+
+import { Button, Container, Paper } from '@mantine/core';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider theme={{
+      colorScheme: 'dark', colors: {
+        'ocean-blue': ['#7AD1DD', '#5FCCDB', '#44CADC', '#2AC9DE', '#1AC2D9', '#11B7CD', '#09ADC3', '#0E99AC', '#128797', '#147885'],
+        'bright-pink': ['#F0BBDD', '#ED9BCF', '#EC7CC3', '#ED5DB8', '#F13EAF', '#F71FA7', '#FF00A1', '#E00890', '#C50E82', '#AD1374'],
+      },
+    }} withGlobalStyles withNormalizeCSS>
+      <Center mx="auto" className="main">
+        <Stack >
+          <Title order={1} className="title" color="ocean-blue">Hello, im Freddy! 👋</Title>
+          <Text>Welcome to my portfolio.</Text>
+        </Stack>
+        <div className='readmore'>
+          <Text>Learn more about me..</Text>
+          <FaArrowDown className='arrowdown' />
+        </div>
+      </Center>
+    </MantineProvider>
   );
 }
 
